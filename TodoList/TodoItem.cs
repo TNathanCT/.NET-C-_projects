3e3ba75ba8f5dfdab062ae33b6aff2085d3de0bc
Thomas Nathan CT <32567705+TNathanCT@users.noreply.github.com>
2025-05-30 14:04:44 +0100

    Update TodoItem.cs

--- TodoList/TodoItem.cs
+++ TodoList/TodoItem.cs
@@ -1,10 +1,14 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Text.Json;
 using System.Collections.Generic;
 
 public class TodoItem
 {
-    public string? Description { get; set; }
+    //this ensures that the string cannot be null
+    public required string Description { get; set; }
     public bool Done { get; set; }
+
+    public DateTime? dueDate { get; set; } 
+    //This is optional
 }
