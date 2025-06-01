6bb1c633418cc8e4e8604b3cd537b29168db4524
Thomas Nathan CT <32567705+TNathanCT@users.noreply.github.com>
2025-06-01 15:24:52 +0100

    Update TodoItem.cs

--- TodoList/TodoItem.cs
+++ TodoList/TodoItem.cs
@@ -8,7 +8,15 @@ public class TodoItem
     //this ensures that the string cannot be null
     public required string Description { get; set; }
     public bool Done { get; set; }
-
-    public DateTime? dueDate { get; set; } 
+    public DateTime? dueDates { get; set; } 
     //This is optional
+    public Priority priority { get; set; } = Priority.Medium;
+    public List<string> tags { get; set; } = new();
+    
+}
+
+public enum Priority{
+    Low,
+    Medium,
+    High
 }
