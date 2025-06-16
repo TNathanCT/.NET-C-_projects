99c941d56f959eac58a7956261f2ede131c6bdf2
Thomas Nathan CT <32567705+TNathanCT@users.noreply.github.com>
2025-06-16 09:22:08 +0100

    Update Program.cs

--- CreatingAPI/Program.cs
+++ CreatingAPI/Program.cs
@@ -1,20 +1,76 @@
 using System;
+using System.Collections.Generic;
 
-class Program{
-    static void Main(string[] args){
-        Console.WriteLine("/Write your email here: ");
-        var inputEmail = Console.ReadLine();
-        if(String.IsNullOrEmpty(inputEmail)){
-            
-        }
+public class Program{
+    
+    //the list of tasks we have saved
+    public List<string> taskList = new List<string>();
+
+    //the list of actions we can perform. Modify here to facilitate future additions
+    public List<string> modificationsPermitted = new List<string>(){"add" , "tick"};
+    
+    public static void Main(string[] args){
         
-        Console.WriteLine("/Write your name here: ");
-        var inputName = Console.ReadLine();
+        var program = new Program();
+
+        //prevent a null or wrong input
+        while(true){
+            //this will need to slightly changed if we want to add future options.
+            Console.WriteLine("Do you wish to add new tasks or check the as complete? Please  type 'add' or 'tick'.");
+            var input = Console.ReadLine()?.Trim().toLower();
+
+            if(input == null ||  string.IsNullOrEmpty(input)){
+                Console.WriteLine("No input detected. Please Try again");
+                continue;
+            }
+
+            if(modificationsPermitted.Contains(input)){
+                break;
+            }
+            else{
+                Console.WriteLine("Error: Wrote input. Please try again");
+            }
+
+        }
+
+        //this will also need to be changed.
+        switch(input){
+            case "add":
+            if(taskList.Count == 0){}
 
 
-        var newUser = new UserAccount(inputName, inputEmail);
-        Console.WriteLine("Is Email Valid? " + newUser.IsEmailValid());
-        Console.WriteLine("Created at: " + newUser.createdAt);
+        }
 
+
+
+        //intialise the first three tasks by asking the user what they are
+        for(int i = 0; i < 3; i++){
+            Console.WriteLine("Task Number " + (i+1) + " : What do you need to do");
+            var input = Console.ReadLine();
+            var task = new TodoItem(input, false);
+            program.AddTask(input);
+            Console.WriteLine("Task Number " + (i+1) + " added. Add " + (3 - (i+1)) + " more");       
+        }      
+        program.DisplayList();
+    }
+
+
+    public void AddTask(string taskName){
+        taskList.Add(taskName);
+    }
+    public void DisplayList(){
+        for(int i = 0; i <= taskList.Count-1; i++){
+            Console.WriteLine("[] {0}",taskList[i]);
+        }
+    }
+}
+
+public class TodoItem{
+    public string TaskName { get; set;}
+    public bool IsDone{ get; set;}
+
+    public TodoItem(string name, bool done){
+        TaskName = name;
+        IsDone = done;
     }
 }
