6ded20a9e8839a8d61033c9918a6dac4b05b5ed2
Thomas Nathan CT <32567705+TNathanCT@users.noreply.github.com>
2025-06-16 12:06:42 +0100

    Update Program.cs

--- CreatingAPI/Program.cs
+++ CreatingAPI/Program.cs
@@ -4,27 +4,26 @@ using System.Collections.Generic;
 public class Program{
     
     //the list of tasks we have saved
-    public List<string> taskList = new List<string>();
+    public List<TodoItem> taskList = new List<TodoItem>();
 
     //the list of actions we can perform. Modify here to facilitate future additions
-    public List<string> modificationsPermitted = new List<string>(){"add" , "tick"};
+    public List<string> modificationsPermitted = new List<string>(){"add" , "tick", "list", "rename"};
     
     public static void Main(string[] args){
         
         var program = new Program();
+        //this will need to slightly changed if we want to add future options.
+        Console.WriteLine("Do you wish to add new tasks or check the as complete? Please  type 'add', 'list', 'rename' or 'tick'.");
+        var input = Console.ReadLine()?.Trim().ToLower();
 
         //prevent a null or wrong input
         while(true){
-            //this will need to slightly changed if we want to add future options.
-            Console.WriteLine("Do you wish to add new tasks or check the as complete? Please  type 'add' or 'tick'.");
-            var input = Console.ReadLine()?.Trim().toLower();
-
-            if(input == null ||  string.IsNullOrEmpty(input)){
+            if(input == null || string.IsNullOrEmpty(input)){
                 Console.WriteLine("No input detected. Please Try again");
                 continue;
             }
 
-            if(modificationsPermitted.Contains(input)){
+            if(program.modificationsPermitted.Contains(input)){
                 break;
             }
             else{
@@ -33,34 +32,82 @@ public class Program{
 
         }
 
+
+
+
+
+
+
+
+
+
         //this will also need to be changed.
         switch(input){
             case "add":
-            if(taskList.Count == 0){}
+                program.AddTask();
+                break;
+            case "list":
+                program.DisplayList();
+                break;
+
+            case "tick":
+                program.CompleteTask();
+                program.DisplayList();
+                break;
+
+            default:
+                break;
+
+        }   
+    }
 
 
+    public void AddTask(string defaultValue = "[Unnamed Task]"){
+        //ensure that the first three tasks are set.
+        if(taskList.Count == 0){
+            AddFirstThree();
+            return;//bypass the rest of the function
         }
 
+        Console.WriteLine("What should task number " + taskList.Count + " be ?");
+        var input = Console.ReadLine();
+        input = string.IsNullOrWhiteSpace(input) ? defaultValue : input.Trim();
 
+       //if(input != defaultValue){
+            taskList.Add(new TodoItem (input, false));
+            Console.WriteLine("Task Number " + taskList.Count + " added : " + input);
+        //}   
+    }
 
-        //intialise the first three tasks by asking the user what they are
+    public void AddFirstThree(string defaultValue = "[Unnamed Task]"){
         for(int i = 0; i < 3; i++){
             Console.WriteLine("Task Number " + (i+1) + " : What do you need to do");
             var input = Console.ReadLine();
-            var task = new TodoItem(input, false);
-            program.AddTask(input);
+            input = string.IsNullOrWhiteSpace(input) ? defaultValue : input.Trim();
+            taskList.Add(new TodoItem (input, false));
             Console.WriteLine("Task Number " + (i+1) + " added. Add " + (3 - (i+1)) + " more");       
-        }      
-        program.DisplayList();
+        }     
+    }
+    
+
+
+    public void CompleteTask(){
+        Console.WriteLine("Which task is complete? Please provide the number.");
+        var input = Console.ReadLine();
+        while (!double.TryParse(Console.ReadLine(), out number))
+        {
+            Console.Write("This is not valid input. Please enter an integer value: ");
+        }
+
+        taskList[input].isDone = true;        
     }
 
 
-    public void AddTask(string taskName){
-        taskList.Add(taskName);
-    }
+
+
     public void DisplayList(){
         for(int i = 0; i <= taskList.Count-1; i++){
-            Console.WriteLine("[] {0}",taskList[i]);
+            Console.WriteLine("Task number " + (i+1) + (taskList[i].isDone ? "[X] " : "[ ] ") + taskList[i].Name);
         }
     }
 }
